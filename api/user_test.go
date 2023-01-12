d6723b4517cf0c4a2d50d7bc2871f442132e1552
Vadim Gorbachev <vadim.gorbachev.main@yandex.ru>
2023-01-12 11:08:44 +0300

    cleanup

--- api/user_test.go
+++ api/user_test.go
@@ -132,29 +132,3 @@ func TestCreateUserApi(t *testing.T) {
 		})
 	}
 }
-
-// func TestCreateUserApi(t *testing.T) {
-// 	type fields struct {
-// 		store  db.Store
-// 		router *gin.Engine
-// 	}
-// 	type args struct {
-// 		ctx *gin.Context
-// 	}
-// 	tests := []struct {
-// 		name   string
-// 		fields fields
-// 		args   args
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			s := &Server{
-// 				store:  tt.fields.store,
-// 				router: tt.fields.router,
-// 			}
-// 			s.createUser(tt.args.ctx)
-// 		})
-// 	}
-// }
