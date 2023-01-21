8c92f00ee387f22fc27b912c3ee1d506f1dde0ab
Vadim Gorbachev <vadim.gorbachev.main@yandex.ru>
2023-01-21 06:53:19 +0300

    removed log

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,6 @@ set -e
 
 echo "run db migrations"
 source /app/prod.env
-echo "$(cat /app/prod.env)"
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
 
 echo "start the web server"
